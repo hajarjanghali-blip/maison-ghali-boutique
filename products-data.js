0684da2096ccc2ec36f40a945e7fd288ce7c05eb
hajarjanghali-blip <hajarjanghali-blip@users.noreply.github.com>
2026-07-03 16:28:49 +0200

    add Bracelet Acier 149 DHS

--- products-data.js
+++ products-data.js
@@ -1,5 +1,3 @@
 const products = [
-    // Ajoute tes nouveaux produits ici
-    // Exemple :
-    // { id: 1, name: "Nom du produit", category: "bracelets", price: 12.83, rating: 5, image: "URL_IMAGE", buyers: [], desc: "Description" },
+    { id: 1, name: "Bracelet Acier", category: "bracelets", price: 13.76, rating: 4, image: "https://res.cloudinary.com/dx5d98buj/image/upload/v1783088608/1000004836_hdc33u.png", buyers: [], desc: "Bracelet en acier de haute qualit&eacute;. Design &eacute;l&eacute;gant et intemporel, finition soign&eacute;e. Id&eacute;al pour un look raffin&eacute; au quotidien. Livraison rapide au Maroc." },
 ];
